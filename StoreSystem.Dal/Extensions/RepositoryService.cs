094a800f83c15717813824debaad77d538e949aa
Vitalii Voitovych <vitalikvv2119@gmail.com>
2023-06-09 16:18:56 +0300

    Created a header with a menu for the site. Repositories are added through a separate extension method

---
+++ StoreSystem.Dal/Extensions/RepositoryService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StoreSystem.Dal.Repositories;
+
+namespace StoreSystem.Dal.Extensions;
+
+public static class RepositoryService
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerRepository, CustomerRepository>()
+                .AddScoped<ISellerRepository, SellerRepository>()
+                .AddScoped<IProductRepository, ProductRepository>()
+                .AddScoped<IOrderRepository, OrderRepository>()
+                .AddScoped<IOrderRecordRepository, OrderRecordRepository>()
+                .AddScoped<ISellRepository, SellRepository>();
+
+        return services;
+    }
+}
--- StoreSystem.Web/Program.cs
+++ StoreSystem.Web/Program.cs
@@ -1,22 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using StoreSystem.Dal.Extensions;
-using StoreSystem.Dal.Repositories;
-using StoreSystem.Dal.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder.Services.AddStoreSystemDbContext(connectionString);
-
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ISellerRepository, SellerRepository>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderRecordRepository, OrderRecordRepository>();
-builder.Services.AddScoped<ISellRepository, SellRepository>();
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.MapGet("/", () => "Hello World");
 
 app.Run();
